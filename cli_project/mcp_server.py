4e09f84e30c29b49eed005f38b79b1f6f8e06fbb
Daniel Sierra <danylang77@gmail.com>
2026-06-03 23:01:42 -0600

    Adding edit function

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -19,13 +19,26 @@ docs = {
     description="Read the contents of a document and return it as a string"
 )
 def read_document(
-    doc_id: str = Field(description"Id of the document to read")
+    doc_id: str = Field(description="Id of the document to read")
 ):
     if doc_id not in docs:
         raise ValueError(f"Doc with id {doc_id} not found")
         return docs[doc_id]
 
 # TODO: Write a tool to edit a doc
+@mcp.tool(
+    name="edit_document",
+    description="Edit a document by replacing a string in the documents content with a new string"
+)
+def edit_document(
+    doc_id:str = Field(description="Id of the document that will be edited"),
+    old_str:str = Field(description="The text to replace. Must match exactly including whitespaces"),
+    new_str:str = Field(description="The text to replace. Must match exactly including whitespaces")
+)
+    if doc_id not in docs:
+        raise ValueError(f"Doc with id {doc_id} not found")
+        return docs[doc_id]
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
