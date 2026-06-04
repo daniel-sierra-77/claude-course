9717874f2d401355a56d890b9ea95641a176294c
Daniel Sierra <danylang77@gmail.com>
2026-06-03 23:03:05 -0600

    test commit

--- cli_project/mcp_server.py
+++ cli_project/mcp_server.py
@@ -39,6 +39,7 @@ def edit_document(
         raise ValueError(f"Doc with id {doc_id} not found")
         return docs[doc_id]
 
+
 # TODO: Write a resource to return all doc id's
 # TODO: Write a resource to return the contents of a particular doc
 # TODO: Write a prompt to rewrite a doc in markdown format
